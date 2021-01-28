f0b8bd8fdb3f3c7943947f309c79fa441826b02b
Pete Angjelevski <p.angjelevski@gmail.com>
2021-01-29 10:11:23 +1300

    add datatbase

---
+++ bodyPartDatabase.js
@@ -0,0 +1,163 @@
+const body = {
+    arms: [
+        {
+            id: 1,
+            name: 'Hammer Curls',
+            description: 'Hold a dumbbell in each hand with palms facing your sides and arms extended straight down. Keeping your upper arms against your sides, curl both weights at the same time, minimizing momentum used during the curl.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_hammercheatcurl.jpg?quality=86&strip=all'
+        },
+        {
+            id: 2,
+            name: 'Dips',
+            description: 'Use dip bars, if available, or place your palms on a bench, chair, or on the floor as you extend your legs in front of you. Lower your body until your upper arms are parallel to the floor, but no lower. Extend your elbows to come up',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_dips_0.jpg?quality=86&strip=all'
+        },
+        {
+            id: 3,
+            name: 'Diamond Pushup',
+            description: 'et into pushup position but place your hands close together so your thumbs and index fingers touch. Keeping your body in a straight line with abs braced, lower your torso until your chest is just above the floor, then press back up.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/30-best-arm-exercises-diamond-pushup.jpg?quality=86&strip=all'
+        },
+        {
+            id: 4,
+            name: 'EZ-Bar Preacher Curl',
+            description: 'Sit at a preacher bench and adjust the height so that your armpits touch the top of the bench. Grasp an EZ-curl bar at shoulder width with arms extended (but allow a slight bend at the elbows). Curl the bar, keeping the backs of your arms against the bench. Take three seconds to lower the bar back down',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_ezbarpreachercurl.jpg?quality=86&strip=all'
+        },
+        {
+            id: 5,
+            name: 'Pullover/Triceps Extension',
+            description: 'Hold the bar with an overhand, shoulder-width grip while lying on a flat bench. Press the bar toward the ceiling and then reach it back over your head while bending your elbows until you feel a stretch in your lats. Then pull the bar back over your chest and extend your elbows. That’s one rep.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/30-best-arm-exercises-pullover-tricep-extension.jpg?quality=86&strip=all'
+        },
+       
+    ],
+    legs: [
+        {
+        id: 1,
+        name: 'Front Squat',
+        description: 'Set a barbell on a power rack at about shoulder height. Grab the power with an overhand grip at shoulder width and raise your elbows until your upper arms are parallel to the floor. Take the bar out of the rack and let it rest on your fingertips. Your elbows should be all the way up throughout the movement. Step back and set your feet at shoulder width with toes turned out slightly. Squat as low as you can without losing the arch in your lower back.',
+        image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_frontsquat_1.jpg?quality=86&strip=all'
+    },
+    {
+        id: 2,
+        name: 'Bulgarian Split Squat',
+        description: 'Stand lunge-length in front of a bench. Hold a dumbbell in each hand and rest the top of your left foot on the bench behind you. Lower your body until your rear knee nearly touches the floor and your front thigh is parallel to the floor. Single-leg training can yield serious strength gains. ',
+        image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_bulgariansplitsquat.jpg?quality=86&strip=all'
+    },
+    {
+        id: 3,
+        name: 'Romanian Deadlift',
+        description: 'A killer deadlift variation, hold a barbell with a shoulder-width grip and stand with feet hip-width apart. Bend your hips back as far as you can. Allow your knees to bend as needed while you lower the bar along your shins until you feel a stretch in your hamstrings. Keep your lower back in its natural arched position throughout.',
+        image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_romaniandeadlift.jpg?quality=86&strip=all'
+    },
+    {
+        id: 4,
+        name: 'Squat',
+        description: 'In a squat rack or cage, grasp the bar as far apart as is comfortable and step under it. Place it on your lower traps, squeeze your shoulder blades together, push your elbows up and nudge the bar out of the rack. Take a step or two back and stand with your feet at shoulder width and your toes turned slightly out. Take a deep breath and bend your hips back, then bend your knees to lower your body as far as you can without losing the arch in your lower back. Push your knees out as you descend. Drive vertically with your hips to come back up, continuing to push your knees out.',
+        image: 'https://www.mensjournal.com/wp-content/uploads/mf/squat_main_1.jpg?quality=86&strip=all'
+    },
+    {
+        id: 5,
+        name: 'Deadlift',
+        description: 'Stand straight up with feet hip-width apart and shins one inch away from the bar. Grip the bar with a double pronated or reverse grip, bend knees and push them into your straight arms. Bring your chest up as much as possible and look straight ahead. Keeping your back flat, extend your hips to stand up, pulling the bar up along your legs to lockout. Here are 4 ways to break your deadlift record.',
+        image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_deadlift_1.jpg?quality=86&strip=all'
+    },
+    ],
+    chest: [
+        {
+            id: 1,
+            name: 'Barbell Bench Press',
+            description: 'Grasp the bar just outside shoulder-width and arch your back so there’s space between your lower back and the bench. Pull the bar out of the rack and lower it to your sternum, tucking your elbows about 45° to your sides. When the bar touches your body, drive your feet hard into the floor and press the bar back up.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main2_benchpress.jpg?quality=86&strip=all'
+        },
+        {
+            id: 2,
+            name: 'Dumbbell Bench Press',
+            description: 'Lie back on a flat bench with a dumbbell in each hand. Hold the weights at shoulder-level, then press the weights straight up.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/dumbbell-bench-press-chest-weights-2-exercise_landscape.jpg?quality=86&strip=all'
+        },
+        {
+            id: 3,
+            name: 'Cable Crossover',
+            description: 'Stand between two facing cable stations with both pulleys set midway between the top and bottom of the station. Attach a D-handle to each pulley and hold one in each hand. Keep your elbows slightly bent, and step forward so there’s tension on the cables. Flex your pecs as you bring your hands together out in front of your chest. Alternate stretching and flexing after each set.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_cablecrossover.jpg?quality=86&strip=all'
+        },
+        {
+            id: 4,
+            name: 'Incline Dumbbell Press',
+            description: 'Set an adjustable bench to a 30°-45° angle and lie back on it with a dumbbell in each hand at shoulder-level. Press the weights over your chest.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/incline_dumbbell_press_pos_b_1.jpg?quality=86&strip=all'
+        },
+        {
+            id: 5,
+            name: 'Dumbbell Flye',
+            description: 'Lie back on a flat bench with a dumbbell in each hand. Keep a slight bend in your elbows and spread your arms wide, lowering the weights until they’re even with your chest. Flex your pecs and lift the weights back to the starting position.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/1280-dumbbell-flye.jpg?quality=86&strip=all'
+        },
+    ],
+    back: [
+        {
+            id: 1,
+            name: 'Weighted Pullup',
+            description: 'Attach a weighted belt to your waist, hold a dumbbell between your feet, or—if you can’t complete your reps with weight—use body weight alone. Hang from a pullup bar with your hands just outside shoulder width. Pull yourself up until your chin is over the bar.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/weighted_pullup.jpg?quality=86&strip=all'
+        },
+        {
+            id: 2,
+            name: 'Incline Dumbbell Row',
+            description: 'Set an adjustable bench to a 30- to 45-degree incline and lie on it chest-down. Grasp a dumbbell in each hand and draw your shoulder blades back and together as you row the weights to your sides.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_inclinedbrow.jpg?quality=86&strip=all'
+        },
+        {
+            id: 3,
+            name: 'Chinup',
+            description: 'Hang from a chinup bar with hands shoulder-width apart using a supinated grip (so, palms facing you). From the bottom of the movement, pull yourself up until your chin is over the bar.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_chinup.jpg?quality=86&strip=all'
+        },
+        {
+            id: 4,
+            name: 'Inverted Row',
+            description: 'Set a barbell in a power rack (or use a Smith machine) at about hip height. (About four rungs up should work). Lie underneath it and grab it with hands about shoulder-width apart with the bottom of your heels on floor. Hang from the bar so your body forms a straight line. Squeeze your shoulder blades together and pull yourself up until your back is fully contracted.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_inverted_row.jpg?quality=86&strip=all'
+        },
+        {
+            id: 5,
+            name: 'Yates Row',
+            description: 'Hold the bar with an underhand grip at shoulder-width. Keeping your lower back in its natural arch, bend your hips back and lower your torso to about 60 degrees. Row the weight to your belly button using a slight cheat (use momentum to begin each rep), but don’t let your lower back round. If you have wrist straps, you may use them to help your grip.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/30-best-back-workouts-yates-row.jpg?quality=86&strip=all'
+        },
+    ],
+    core: [
+        {
+            id: 1,
+            name: 'Arms-High Partial Situp',
+            description: 'Lie on your back, knees bent at 90 degrees, and raise your arms straight overhead, keeping them pointing up throughout the exercise. Sit up halfway, then steadily return to the floor. That’s one rep.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/30-best-ab-workouts-arms-high-partial-situp.jpg?quality=86&strip=all'
+        },
+        {
+            id: 2,
+            name: 'Flutter Kick',
+            description: 'Lie on your back with legs straight and arms extend out at your sides. Lift your heels about 6 inches off the floor and rapidly kick your feet up and down in a quick, scissor-like motion.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/30-best-ab-workouts-flutter-kick.jpg?quality=86&strip=all'
+        },
+        {
+            id: 3,
+            name: 'Leg Raise',
+            description: 'Lie on the floor and hold onto a bench or the legs of a heavy chair for support. Keep your legs straight and raise them up until they’re vertical. Lower back down, but stop just short of the floor to keep tension on your abs before the next rep.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_legraise_1.jpg?quality=86&strip=all'
+        },
+        {
+            id: 4,
+            name: 'Medicine Ball Russian Twist',
+            description: 'Sit on the floor in the top position of a situp and, holding a medicine ball with both hands, extend your arms in front of you. Explosively twist your body to one side and then twist back. Alternate sides',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_russiantwist.jpg?quality=86&strip=all'
+        },
+        {
+            id: 5,
+            name: 'Plank',
+            description: 'Get into pushup position and bend your elbows to lower your forearms to the floor. Hold the position with abs braced.',
+            image: 'https://www.mensjournal.com/wp-content/uploads/mf/_main_plank_4.jpg?quality=86&strip=all'
+        },
+    ]
+}
