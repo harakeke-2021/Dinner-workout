5690794fb2072d69013ef18c2fb482476b3a8ccf
steve-kim-96 <soungku@hotmail.com>
2021-01-29 10:38:21 +1300

    update homepage setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+    <Route exact path='/' component={Home} />
+    </>
   )
 }
 
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
-function Home () {}
+import BodyParts from './BodyParts'
+
+function Home () {
+    return (
+        <div>
+            <h3>Home</h3>
+            <p>Select a body part from below</p>
+        </div>
+    )
+}
 
 export default Home
